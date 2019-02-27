c21d297479d87a3431b1fbcbeec13e59c470bf06
kindluddite <howley.phil@gmail.com>
2019-02-27 11:56:35 -0800

    change about

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
-    <h1>About Me</h1>
+    <h1>About</h1>
     <p>My philosophy on life is YOLO</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Skateboard />
